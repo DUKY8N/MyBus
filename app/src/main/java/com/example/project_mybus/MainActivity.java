dedfd1d87e32ee28e05be25dcbfdfe26ec692927
Dukyoon Im <DUKY8N@gmail.com>
2020-11-22 06:40:17 +0900

    beta 1.0.4

--- app/src/main/java/com/example/project_mybus/MainActivity.java
+++ app/src/main/java/com/example/project_mybus/MainActivity.java
@@ -13,12 +13,14 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private FloatingActionButton fab, fab1, fab2, fab3;
 
     TextView tv;
+    String test = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,12 +55,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv = (TextView)findViewById(R.id.tv);
 
         // 대기오염 정보 API
-        String api = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList?serviceKey=%2FnU0vVe9yEqaJ2vRtCPpJZHv%2Bef81aaG8G2pMXgYpYhJGqpcVzsFP2pqQ62JPlcfY54It2FZeXgN3p8nItuu9Q%3D%3D&cityCode=25&nodeId=DJB8001793&routeId=DJB30300002";
+        String api = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getCtyCodeList";
+        String key = "%2FnU0vVe9yEqaJ2vRtCPpJZHv%2Bef81aaG8G2pMXgYpYhJGqpcVzsFP2pqQ62JPlcfY54It2FZeXgN3p8nItuu9Q%3D%3D";
+        String cityCode = "25";
+        String nodeId = "DJB8001793";
+//        String url = api + "?serviceKey=" + key + "&cityCode=" + cityCode + "&nodeId=" + nodeId;
+        String url = api + "?serviceKey=" + key;
 
         // API를 이용한 데이터 다운로드 객체
         DownloadWebpageTask task = new DownloadWebpageTask();
         // 데이터 다운로드 및 처리
-        task.execute(api);
+        task.execute(url);
     }
 
     // 데이터 다운로드 클래스 정의
@@ -77,6 +85,70 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 문서 다운로드 후 자동 호출: MXL 문서 파싱
         protected void onPostExecute(String result) {
+            boolean bSet_itemCode = false;
+            boolean bSet_city = false;
+
+            String itemCode = "";
+            String pollution_degree = "";
+            String tag_name = "";
+
+            int cnt = 0;
+            int city_no = 0;
+
+            try {
+                // XML Pull Parser 객체 생성
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(true);
+                XmlPullParser xpp = factory.newPullParser();
+
+                // 파싱할 문서 설정
+                xpp.setInput(new StringReader(result));
+
+                // 현재 이벤트 유형 반환(START_DOCUMENT, START_TAG, TEXT, END_TAG, END_DOCUMENT
+                int eventType = xpp.getEventType();
+
+                // 이벤트 유형이 문서 마지막이 될 때까지 반복
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    // 문서의 시작인 경우
+                    if (eventType == XmlPullParser.START_DOCUMENT) {
+                        ;
+
+                        // START_TAG이면 태그 이름 확인
+                    } else if (eventType == XmlPullParser.START_TAG) {
+                        tag_name = xpp.getName();
+                        if (bSet_itemCode == false && tag_name.equals("citycode"))
+                            bSet_itemCode = true;
+                        if (itemCode.equals("군포시") && (tag_name.equals("울산시") || tag_name.equals("busan")))
+                            bSet_city = true;
+
+                        // 태그 사이의 문자 확인
+                    } else if (eventType == XmlPullParser.TEXT) {
+                        if (bSet_itemCode) {
+                            itemCode = xpp.getText();
+
+                            if (itemCode.equals("PM10")) {
+                                cnt++;
+                                bSet_itemCode = false;
+                            }
+                        }
+                        if (bSet_city) {
+                            pollution_degree = xpp.getText();
+
+                            // 도시와 미세먼지 농도 화면 출력
+                            tv.append("" + cnt + ": " + tag_name + ", " + pollution_degree + "\n");
+                            bSet_city = false;
+                        }
+
+                        // 마침 태그인 경우
+                    } else if (eventType == XmlPullParser.END_TAG) {
+                        ;
+                    }
+
+                    // 다음 이벤트 유형 할당
+                    eventType = xpp.next();
+                }
+            } catch (Exception e) {
+            }
             tv.setText(result);
         }
 
@@ -95,6 +167,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String page = "";
                 while ((line = bufreader.readLine()) != null) {
                     page += line;
+                    test += line;
                 }
 
                 // 다운로드 문서 반환
