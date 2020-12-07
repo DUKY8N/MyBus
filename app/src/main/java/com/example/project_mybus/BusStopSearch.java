966f5cd5ef1f760c8b4f8530cb5b7e47ad0be1b7
cho seok jin <cba64511@gmail.com>
2020-12-07 20:21:32 +0900

    beta 1.1.4

--- app/src/main/java/com/example/project_mybus/BusStopSearch.java
+++ app/src/main/java/com/example/project_mybus/BusStopSearch.java
@@ -3,19 +3,61 @@ package com.example.project_mybus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 public class BusStopSearch extends AppCompatActivity implements View.OnClickListener {
+
+    String serviceKey = "%2FnU0vVe9yEqaJ2vRtCPpJZHv%2Bef81aaG8G2pMXgYpYhJGqpcVzsFP2pqQ62JPlcfY54It2FZeXgN3p8nItuu9Q%3D%3D";
+    int cityCode;
+    String nodeNm;
+    int nodeNo;
+    int numOfRows = 30;
+    int pageNo = 1;
 
     private Animation fab_open, fab_close;
     private Boolean isFabOpen = false;
     private FloatingActionButton fab, fab1, fab2, fab3;
+
+    Document doc;
+    LinearLayout dynamicLayout;
+    LinearLayout dynamicHori;
+    String[] bstop_nm;
+    String bstop_no;
+    String[] bstop_id;
+    int count;
+    int idc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +77,144 @@ public class BusStopSearch extends AppCompatActivity implements View.OnClickList
         fab1.setOnClickListener(this);
         fab2.setOnClickListener(this);
         fab3.setOnClickListener(this);
+
+        EditText edt = (EditText) findViewById(R.id.search);
+        edt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                switch (actionId) {
+                    case EditorInfo.IME_ACTION_SEARCH:
+                        EditText searchEditText = (EditText)findViewById(R.id.search);
+                        //nodeNm = Integer.parseInt(searchEditText.getText().toString());
+                        nodeNm = searchEditText.getText().toString();
+                        dynamicLayout = (LinearLayout)findViewById(R.id.dynamicLayout);
+                        dynamicLayout.removeAllViews();
+                        new GetXMLTask().execute();
+                        Toast.makeText(getApplicationContext(), "로딩 중...", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) {
+                    cityCode = 37020;
+                }
+                else if(position == 1) {
+                    cityCode = 31230;
+                }
+                else if(position == 2) {
+                    cityCode = 26;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                cityCode = 37020;
+            }
+        });
+    }
+
+    private class GetXMLTask extends AsyncTask<String, Void, Document> {
+        @Override
+        protected Document doInBackground(String... urls) {
+            URL url;
+            try {
+                url = new URL("http://openapi.tago.go.kr/openapi/service/BusSttnInfoInqireService/getSttnNoList"+
+                        "?serviceKey=" + serviceKey +
+                        "&cityCode="+ cityCode +
+                        "&nodeNm=" + nodeNm +
+                        "&numOfRows" + numOfRows +
+                        "pageNo" + pageNo);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                doc = db.parse(new InputSource(url.openStream()));
+                doc.getDocumentElement().normalize();
+
+            } catch (Exception e) {
+                Toast.makeText(getBaseContext(), "Parsing Error", Toast.LENGTH_SHORT).show();
+            }
+            return doc;
+        }
+
+        @Override
+        protected void onPostExecute(Document doc) {
+
+            NodeList nodeList = doc.getElementsByTagName("item");
+            idc = 0;
+            bstop_nm = new String[nodeList.getLength()];
+            bstop_id = new String[nodeList.getLength()];
+
+            for(int i = 0; i< nodeList.getLength(); i++){
+
+                Node node = nodeList.item(i);
+                Element fstElmnt = (Element) node;
+
+                NodeList nodenm = fstElmnt.getElementsByTagName("nodenm");
+                bstop_nm[i] = nodenm.item(0).getChildNodes().item(0).getNodeValue();
+                NodeList nodeno = fstElmnt.getElementsByTagName("nodeno");
+                bstop_no = "(" + nodeno.item(0).getChildNodes().item(0).getNodeValue() + ")";
+                NodeList nodeid = fstElmnt.getElementsByTagName("nodeid");
+                bstop_id[i] = nodeid.item(0).getChildNodes().item(0).getNodeValue();
+
+                idc++;
+                AddText(bstop_nm[i], bstop_no, idc);
+            }
+
+            super.onPostExecute(doc);
+        }
+    }
+
+    public void AddText(String s1, String s2, int id) {
+        dynamicLayout = (LinearLayout)findViewById(R.id.dynamicLayout);
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT, 2.0f);
+        param.width = MATCH_PARENT;
+        param.height = 250;
+        param.leftMargin = 50;
+        param.rightMargin = 50;
+        param.topMargin = 40;
+        param.bottomMargin = 10;
+        TextView stopnm_tv = new TextView(this);
+        TextView stopnum_tv = new TextView(this);
+        ImageView stopicon_v = new ImageView(this);
+        stopnm_tv.setText(s1);
+        stopnm_tv.setTextSize(25);
+        stopnm_tv.setEllipsize(TextUtils.TruncateAt.END);
+        stopnm_tv.setSingleLine(true);
+        stopnum_tv.setText(s2);
+        stopnum_tv.setTextColor(Color.RED);
+        stopicon_v.setImageResource(R.drawable.bus_stop_icon2);
+        stopicon_v.setAdjustViewBounds(true);
+        stopicon_v.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        stopicon_v.setPadding(60, 0, 0, 0);
+        LinearLayout layout_tv = new LinearLayout(this);
+        layout_tv.setOrientation(LinearLayout.VERTICAL);
+        layout_tv.setPadding(50, 40,0,0);
+        layout_tv.setGravity(Gravity.CENTER);
+        layout_tv.addView(stopnm_tv);
+        layout_tv.addView(stopnum_tv);
+        dynamicHori = new LinearLayout(this);
+        dynamicHori.setBackgroundResource(R.drawable.search_menu_shape);
+        dynamicHori.setLayoutParams(param);
+        dynamicHori.addView(stopicon_v);
+        dynamicHori.addView(layout_tv);
+        dynamicHori.setId(id);
+        dynamicHori.setOnClickListener(this);
+        dynamicLayout.addView(dynamicHori);
+    }
+
+    public void SearchBusStop (View v) {
+        EditText searchEditText = (EditText)findViewById(R.id.search);
+        //routeNo = Integer.parseInt(searchEditText.getText().toString());
+        nodeNm = searchEditText.getText().toString();
+        dynamicLayout = (LinearLayout)findViewById(R.id.dynamicLayout);
+        dynamicLayout.removeAllViews();
+        new GetXMLTask().execute();
+        Toast.makeText(getApplicationContext(), "로딩 중...", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -91,5 +271,18 @@ public class BusStopSearch extends AppCompatActivity implements View.OnClickList
         super.onDestroy();
         overridePendingTransition(0,0);
     }
-}
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(event.getAction() == KeyEvent.ACTION_DOWN) {
+            if(keyCode == KeyEvent.KEYCODE_BACK) {
+                Toast.makeText(this, "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+                count++;
+                if(count > 1) {
+                    finish();
+                }
+            }
+        }
+        return true;
+    }
+}
