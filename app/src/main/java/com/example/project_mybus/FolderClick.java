fb1264e97d117d56b8c7a888d50b61724d6a97bb
cho seok jin <cba64511@gmail.com>
2020-12-11 02:52:09 +0900

    beta 1.2.8

--- app/src/main/java/com/example/project_mybus/FolderClick.java
+++ app/src/main/java/com/example/project_mybus/FolderClick.java
@@ -6,8 +6,15 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class FolderClick extends AppCompatActivity {
 
@@ -25,7 +32,6 @@ public class FolderClick extends AppCompatActivity {
         try {
             dbManager = new DBManager(this);
             sqlitedb = dbManager.getReadableDatabase();
-            str_folder = ;
             Cursor cursor = sqlitedb.query("BusBookMark", null, "folder = ?", new String[]{str_folder}, null, null, null);
             while (cursor.moveToNext()) {
                 String busnum = cursor.getString(cursor.getColumnIndex("busnum"));
@@ -42,5 +48,61 @@ public class FolderClick extends AppCompatActivity {
         } catch (SQLiteException e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void AddText(String busnum, String cityid, String startnm, String endnm) {
+        dynamicLayout = (LinearLayout)findViewById(R.id.dynamicLayout);
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT, 2.0f);
+        param.width = MATCH_PARENT;
+        param.height = 200;
+        param.leftMargin = 50;
+        param.rightMargin = 50;
+        param.topMargin = 40;
+        param.bottomMargin = 10;
+        TextView busnum_tv = new TextView(this);
+        TextView startnm_tv = new TextView(this);
+        TextView endnm_tv = new TextView(this);
+        ImageView busicon_v = new ImageView(this);
+        if((cityid.equals("37020")) || (cityid.equals("31230"))){
+            busnum_tv.setText(busnum + "번 버스");
+        }
+        else {
+            busnum_tv.setText(busnum);
+        }
+        busnum_tv.setTextSize(25);
+        busnum_tv.setEllipsize(TextUtils.TruncateAt.END);
+        busnum_tv.setSingleLine(true);
+        busnum_tv.setPadding(50, 30, 0, 0);
+
+        startnm_tv.setText(startnm + " -> ");
+        startnm_tv.setTextSize(15);
+        endnm_tv.setText(endnm);
+        endnm_tv.setTextSize(15);
+        endnm_tv.setEllipsize(TextUtils.TruncateAt.END);
+        endnm_tv.setSingleLine(true);
+
+        LinearLayout layout_tv2 = new LinearLayout(this);
+        layout_tv2.setOrientation(LinearLayout.HORIZONTAL);
+        layout_tv2.setGravity(Gravity.CENTER);
+        layout_tv2.setPadding(30, 0, 0, 0);
+        layout_tv2.addView(startnm_tv);
+        layout_tv2.addView(endnm_tv);
+
+        LinearLayout layout_tv = new LinearLayout(this);
+        layout_tv.setOrientation(LinearLayout.VERTICAL);
+        layout_tv.setGravity(Gravity.CENTER);
+        layout_tv.addView(busnum_tv);
+        layout_tv.addView(layout_tv2);
+
+        busicon_v.setImageResource(R.drawable.bus_icon3);
+        busicon_v.setAdjustViewBounds(true);
+        busicon_v.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        busicon_v.setPadding(60, 30, 0, 30);
+        dynamicHori = new LinearLayout(this);
+        dynamicHori.setBackgroundResource(R.drawable.search_menu_shape);
+        dynamicHori.setLayoutParams(param);
+        dynamicHori.addView(busicon_v);
+        dynamicHori.addView(layout_tv);
+        dynamicLayout.addView(dynamicHori);
     }
 }
